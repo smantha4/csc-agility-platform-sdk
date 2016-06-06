7b1e119929a076d092c62711bfb4882a14d9c9f7
Dustin Behnke <dustin.behnke@servicemesh.com>
2016-06-06 16:11:25 -0500

    Fix spacing

--- agility.api/src/main/java/com/servicemesh/agility/api/service/StoreProductOperations.java
+++ agility.api/src/main/java/com/servicemesh/agility/api/service/StoreProductOperations.java
@@ -22,7 +22,9 @@ public interface StoreProductOperations
     public void setDeployLocked(LaunchItemDeployment launchItemDeployment) throws Exception;
 
     public void deployApproved(LaunchItemDeployment launchItemDeployment, Integer approverId, String comment) throws Exception;
+
     public void deployApproved(LaunchItemDeployment launchItemDeployment, Integer approverId, String comment, boolean start)
             throws Exception;
+
     public void deployRejected(LaunchItemDeployment launchItemDeployment, String comment) throws Exception;
 }
