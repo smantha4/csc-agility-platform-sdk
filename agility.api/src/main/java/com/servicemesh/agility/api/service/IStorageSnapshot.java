59be56c3a3fc381812c28600918f01f86a27eae7
Anatoly Kofman <Anatoly.Kofman@servicemesh.com>
2016-11-23 13:44:35 -0500

    US9051 API to support delete options

--- agility.api/src/main/java/com/servicemesh/agility/api/service/IStorageSnapshot.java
+++ agility.api/src/main/java/com/servicemesh/agility/api/service/IStorageSnapshot.java
@@ -9,6 +9,8 @@ public interface IStorageSnapshot
     public Task createSnapshot(VolumeStorage storage, VolumeStorageSnapshot snapshot) throws Exception;
 
     public Task revertSnapshot(VolumeStorage storage, VolumeStorageSnapshot snapshot) throws Exception;
+    
+    public Task revertSnapshot(VolumeStorage storage, VolumeStorageSnapshot snapshot, String snapshotDeleteOptions) throws Exception;
 
     public Task deleteSnapshot(VolumeStorage storage, VolumeStorageSnapshot snapshot) throws Exception;
 }
