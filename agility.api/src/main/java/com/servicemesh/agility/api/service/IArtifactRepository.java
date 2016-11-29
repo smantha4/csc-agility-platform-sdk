b7b8f60a6f8f3565301f78fa33b2f75f79b9eb88
Bharath Swaminathan <barumugham@csc.com>
2016-11-29 16:32:50 +0530

    TA17512 : defining interfaces for artifactory adapter

---
+++ agility.api/src/main/java/com/servicemesh/agility/api/service/IArtifactRepository.java
@@ -0,0 +1,46 @@
+package com.servicemesh.agility.api.service;
+
+import java.util.List;
+
+import com.servicemesh.agility.api.Artifact;
+import com.servicemesh.agility.api.ServiceProvider;
+
+/**
+ * Exposes additional operations to manage the following types from com.servicemesh.agility.api package: Project, Environment,
+ * Topology Template, Instance
+ */
+public interface IArtifactRepository
+{
+
+    /**
+     * Return the list of all artifacts for this repository
+     *
+     * @param String
+     * @throws Exception
+     */
+    public List<Artifact> getAllArtifacts(String repoPath) throws Exception;
+
+    /**
+     * Return the list of all service providers of for an artifact
+     *
+     * @throws Exception
+     */
+    public List<ServiceProvider> getArtifactServiceProviders() throws Exception;
+
+    /**
+     * Publish artifact meta data to agility
+     *
+     * @param String
+     * @throws Exception
+     */
+    public void publishArtifactMetadata(Artifact artifact) throws Exception;
+
+    /**
+     * Return the artifact
+     *
+     * @param String
+     * @throws Exception
+     */
+    public byte[] downloadArtifacts(String repoPath) throws Exception;
+
+}
